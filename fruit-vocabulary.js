a25d47b4c26e3d86a4465eedc534d8e76781ccbc
Evelyn Mondares <evelyn28mondares@gmail.com>
2026-06-04 11:08:06 +0800

    fruit-vocabulary

---
+++ fruit-vocabulary.js
@@ -0,0 +1,18 @@
+[
+  {
+    "hint": "https://i.postimg.cc/1zsVjgfG/apple.jpg",
+    "answer": "apple"
+  },
+  {
+    "hint": "https://i.postimg.cc/fTd3W7d8/banana.jpg",
+    "answer": "banana"
+  },
+  {
+    "hint": "https://i.postimg.cc/J7GP1hQN/grape.jpg",
+    "answer": "grape"
+  },
+  {
+    "hint": "https://i.postimg.cc/wjHXsxhK/cherry.jpg",
+    "answer": "cherry"
+  }
+]
